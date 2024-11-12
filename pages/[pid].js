4215a5afe730839f374b9a0388cddd56f0a5c789
nutjatuwit <103878796+nutjatuwit0808@users.noreply.github.com>
2024-11-12 23:28:25 +0700

    update pre-generate page lesson

---
+++ pages/[pid].js
@@ -0,0 +1,52 @@
+import React from "react";
+import path from "path";
+import fs from "fs/promises";
+
+function ProductDetailPage(props) {
+  const { loadedProduct } = props;
+
+    // if(!loadedProduct) {
+    //   return <p>Loading...</p>
+    // }
+  return (
+    <>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </>
+  );
+}
+
+async function getData() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return data;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+
+  const productId = params.pid;
+
+  const data = await getData();
+
+  const product = data.products.find((product) => product.id === productId);
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const data = await getData();
+  const ids = data.products.map((product) => product.id);
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+  return {
+    paths: pathsWithParams, //[{params: {pid: "p1"}}],
+    fallback: false//"blocking",
+  };
+}
+
+export default ProductDetailPage;
